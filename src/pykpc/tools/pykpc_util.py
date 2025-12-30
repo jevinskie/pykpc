a3be89f02c8835d1014514389ee42995f00a670d
Jevin Sweval <jevinsweval@gmail.com>
2025-12-29 23:55:14 -0500

    pykpc skeleton

---
+++ src/pykpc/tools/pykpc_util.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import argparse
+
+
+def real_main(args: argparse.Namespace):
+    print(f"args: {args}")
+
+
+def get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="pykpc")
+    return parser
+
+
+def main():
+    real_main(get_arg_parser().parse_args())
+
+
+if __name__ == "__main__":
+    main()
